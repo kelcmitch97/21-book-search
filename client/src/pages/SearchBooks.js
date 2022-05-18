f592ef8c363195e322e738ddb9b94f14cd3f938e
kelcmitch97 <kelciemitchelmore@gmail.com>
2022-05-18 20:13:21 -0230

    login/signup form

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -2,11 +2,12 @@ import React, { useState, useEffect } from 'react';
 import { Jumbotron, Container, Col, Form, Button, Card, CardColumns } from 'react-bootstrap';
 
 import Auth from '../utils/auth';
-import { saveBook, searchGoogleBooks } from '../utils/API';
+import { searchGoogleBooks } from '../utils/API';
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
 
 import { useMutation } from '@apollo/client';
 import { SAVE_BOOK } from '../utils/mutations';
+import { QUERY_ME } from '../utils/queries';
 
 
 const SearchBooks = () => {
@@ -14,12 +15,11 @@ const SearchBooks = () => {
   const [searchedBooks, setSearchedBooks] = useState([]);
   // create state for holding our search field data
   const [searchInput, setSearchInput] = useState('');
-
   // create state to hold saved bookId values
   const [savedBookIds, setSavedBookIds] = useState(getSavedBookIds());
 
   // mutation for save book
-  const [saveBook] = useMutation(SAVE_BOOK);
+  const [saveBook, { error }] = useMutation(SAVE_BOOK);
 
   // set up useEffect hook to save `savedBookIds` list to localStorage on component unmount
   // learn more here: https://reactjs.org/docs/hooks-effect.html#effects-with-cleanup
@@ -72,13 +72,16 @@ const SearchBooks = () => {
     }
 
     try {
-      await saveBook({
-        variables: { book: bookToSave },
-        update: cache => {
-          const {me} = cache.readyQuery({ query: GET_ME });
-          cache.writeQuery({ query: GET_ME, data: {me: { ...me, savedBooks: [...me.savedBooks, bookToSave ]}}})
-        }
-      });
+      const { data } = await saveBook({
+        variables: { newBook: {...bookToSave }},
+      })
+      // await saveBook({
+      //   variables: { book: bookToSave },
+      //   update: cache => {
+      //     const {me} = cache.readQuery({ query: QUERY_ME });
+      //     cache.writeQuery({ query: QUERY_ME, data: {me: { ...me, savedBooks: [...me.savedBooks, bookToSave ]}}})
+      //   }
+      // });
 
       // if book successfully saves to user's account, save book id to state
       setSavedBookIds([...savedBookIds, bookToSave.bookId]);
